e8de20ce48588b0f6daa3e5fe05888199f3d620f
Greg Farris <greg.farris@magnetic.com>
2016-03-01 01:59:13 -0500

    sonar

--- src/main/java/com/magnatic/DemoApplication.java
+++ src/main/java/com/magnatic/DemoApplication.java
@@ -2,11 +2,15 @@ package com.magnatic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	String one = "1";
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		ConfigurableApplicationContext run =
+				SpringApplication.run(DemoApplication.class, args);
 	}
 }
